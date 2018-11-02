5383e1788c89ece22e40037d629054e258d3a186
ThanhTung1235 <tungltd00602@fpt.edu.vn>
2018-11-02 23:30:28 +0700

    First commit

---
+++ MyNetCoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyNetCoreMVC.Models;
+
+namespace MyNetCoreMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public ProductController(MyDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Products.ToList());
+        }
+        
+        public IActionResult GetList()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Save(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            TempData["Message"] = "Create Succes";
+            return Redirect("Index");
+        }
+
+        public IActionResult Edit(long id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        public IActionResult Update(Product product)
+        {
+            var exitProduct = _context.Products.Find(product.id);
+            if (exitProduct == null)
+            {
+                return NotFound();
+            }
+            exitProduct.name = product.name;
+            exitProduct.price = product.price;
+            _context.Products.Update(exitProduct);
+            _context.SaveChanges();
+            return Redirect("Index");
+        }
+
+        public IActionResult Delete(long id)
+        {
+            var exitsProduct = _context.Products.Find(id);
+            if (exitsProduct == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(exitsProduct);
+            _context.SaveChanges();
+            return Redirect("Index");
+        }
+
+        public IActionResult DeleteAll()
+        {
+
+            return Redirect("Index");
+        }
+    }
+}
---
+++ MyNetCoreMVC/Models/MyDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyNetCoreMVC.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public class Category
+        {
+            public long Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+    }
+}
